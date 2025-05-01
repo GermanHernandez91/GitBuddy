79ded321f37c5257e6f6b2b178bdb8feb278ec47
German <german220291@gmail.com>
2025-05-01 15:58:38 +0100

    Initial Commit

---
+++ GitBuddy/GitBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitBuddyApp.swift
+//  GitBuddy
+//
+//  Created by Germán Hernández del Rosario on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct GitBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitBuddyTests/GitBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitBuddyTests.swift
+//  GitBuddyTests
+//
+//  Created by Germán Hernández del Rosario on 1/5/25.
+//
+
+import Testing
+@testable import GitBuddy
+
+struct GitBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
